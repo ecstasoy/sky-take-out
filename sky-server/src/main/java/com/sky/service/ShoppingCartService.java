7444a33b69fe01f7f3743b6684e934eaa025ac08
Kunhua Huang <huang.kunh@northeastern.edu>
2024-08-08 20:09:27 -0400

    show shopping cart - en

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,8 +1,23 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartService {
 
+    /**
+     * Add to shopping cart
+     *
+     * @param shoppingCartDTO
+     */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * Show shopping cart
+     *
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
 }
